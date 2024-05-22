7c35343a84cbdfd4d5820d9da81714dbf3b5aabe
Cullen <a410615903@gmail.com>
2024-05-22 14:44:41 -0700

    Update dev.next.config.sample Add `shared-commponents package as teamates' use

--- app/(root)/LandingPageView.tsx
+++ app/(root)/LandingPageView.tsx
@@ -13,27 +13,12 @@ export default function IndexPage({
 
     return (
         <main className="min-h-screen bg-gray-100 flex flex-col">
-            <Head>
-                <title>Welcome to My Recruitment Website</title>
-                <meta name="description" content="Find your dream job or your ideal candidate here!" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <header className="bg-white shadow-md py-4">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-                    <Image src="/favicon.ico" alt="Website Logo" className="h-8 w-auto" width={10} height={10} />
-                    <nav>
-                        <a href="#services" className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium">Services</a>
-                        <a href="#testimonials" className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium">Testimonials</a>
-                        <a href="contact" className="text-gray-500 hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-                    </nav>
-                </div>
-            </header>
-
+ 
             <main className="flex-grow">
                 <section className="text-center py-20 bg-white">
-                    <h1 className="text-4xl font-bold text-gray-900">Welcome to MiTech Recruitment Website</h1>
-                    <p className="mt-3 text-gray-500">Your one-stop destination for all recruitment needs.</p>
-                    <h1 className="text-4xl font-bold text-gray-900">Trackable Authentable High Transparency Career Path</h1>
+                    <h1 className="text-4xl font-bold text-gray-900">Welcome to MiTech</h1>
+                    <p className="mt-3 text-gray-500"></p>
+                    <h1 className="text-4xl font-bold text-gray-900"></h1>
 
                     <button className="mt-5 bg-blue-500 text-white px-5 py-2 rounded-md text-lg"
                         onClick={() => { }}
